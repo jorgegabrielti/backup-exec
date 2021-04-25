e952c5449248729f2bc1cde433525b1b8607bc84
jorgegabrielti <jorgegabriel.ti@gmail.com>
2021-04-25 13:46:55 -0300

    [update]Improve function 'make_backup()'

--- backup-agent.sh
+++ backup-agent.sh
@@ -47,42 +47,106 @@ aws_assume_role () {
    export AWS_SESSION_TOKEN=$(grep -E 'SessionToken' /tmp/.assumeRole.tmp | awk '{print $2}' | tr -d '"|,')
 }
 
+### Build a regular file
+regular_file_backup () {
+  # Storage directory
+  if [ ! -d ${STORAGE} ]; then
+       mkdir ${STORAGE}
+  fi 
+    
+  tar zcvf ${STORAGE}/${NAME}.tar.gz ${FILE[*]}
+     
+  ### Call functions 
+  # Checksum
+  hash_checksum ${STORAGE}/${NAME}.tar.gz   
+  
+  # AWS Assume Role
+  #aws_assume_role
+
+  # AWS S3 Sync
+  #aws_s3sync /${STORAGE}/${NAME}.tar.gz ${STORAGE}/${NAME}.tar.gz.${CHECKSUM_TYPE}
+}
+
+### Build MySQL Backup with mysqldump
+sgbd_mysql_backup () {
+  # Storage directory
+  STORAGE="${STORAGE}/mysql"
+  if [ ! -d ${STORAGE} ]; then
+       mkdir ${STORAGE}
+  fi
+
+  for BASE in ${DATABASE}; do
+      # Last binary log
+      LOG_BIN_BEFORE_BACKUP=$(mysql -e "SHOW BINARY LOGS" | tail -n1 | awk '{print $1}')
+
+      SALT=$(date +%d%m%Y%M%S%s%N | md5sum | awk '{print $1}')
+      SALT=${SALT:24}
+
+      # Make backup logical
+      mysqldump --databases ${BASE} --single-transaction -F \
+      --result-file=${STORAGE}/${BASE}/${BASE}-${DATE_TODAY}-${SALT}.sql \
+      --log-error=${STORAGE}/${BASE}/logs/${BASE}-${DATE_TODAY}-error-${SALT}.log \
+      --compression-algorithms=zlib \
+      --dump-date
+
+      LOG_BIN_DURING_BACKUP=$(mysql -e "SHOW BINARY LOGS" | tail -n1 | awk '{print $1}')
+
+      # Make a new binlog file
+      mysql -e "FLUSH LOGS"
+
+      LOG_BIN_AFTER_BACKUP=$(mysql -e "SHOW BINARY LOGS" | tail -n1 | awk '{print $1}')
+
+cat > ${STORAGE}/${BASE}/logs/${BASE}-binlog-${SALT}.log << LOGFILE
+# Logbin before backup
+/var/lib/mysql/${LOG_BIN_BEFORE_BACKUP}
+
+# Order to restore the backup:
+1º ==> ${BASE}-${DATE_TODAY}-${SALT}.sql
+2º ==> ${LOG_BIN_DURING_BACKUP}
+3º ==> ${LOG_BIN_AFTER_BACKUP}
+
+# ****** NOTE *******
+In case this is the last backup prior to a point of failure, it must be followed by all binary logs after it to the point of failure
+
+# Procedure
+mysql -e "source ${STORAGE}/${BASE}/${DATABASE_NAME}-${DATE_TODAY}-${SALT}.sql"
+mysqlbinlog /var/lib/mysql/${LOG_BIN_DURING_BACKUP} > ${LOG_BIN_DURING_BACKUP}.sql && mysql -f < ${LOG_BIN_DURING_BACKUP}.sql
+mysqlbinlog /var/lib/mysql/${LOG_BIN_AFTER_BACKUP} > ${LOG_BIN_AFTER_BACKUP}.sql && mysql < ${LOG_BIN_AFTER_BACKUP}.sql
+LOGFILE
+}
+
 # Load global config and process job file
 process_file () {
-
   source "$1"
   for ((c=0; c <$(wc -l ${2} | cut -d' ' -f1); c++)); do
       JOB_DEF[$c]=$(head -n$(($c+1)) "$2" | tail -n1 | cut -d':' -f2)
       echo ${JOB_DEF[$c]} > /tmp/.job_def_${c}
       JOB[$c]="/tmp/.job_def_${c}"
   done
-
 }
 process_file $@
 
 # Execute task backup
 make_backup () {
-
   for TASK in "$@"; do
-    
       source ${TASK}
-    
-      if [ ! -d ${STORAGE} ]; then
-         mkdir ${STORAGE}
-      fi 
-    
-      tar zcvf ${STORAGE}/${NAME}.tar.gz ${FILE[*]}
-      
-      ### Call functions
-      
-      # Checksum
-      hash_checksum ${STORAGE}/${NAME}.tar.gz
-      
-      # AWS Assume Role
-      aws_assume_role
-
-      # AWS S3 Sync
-      aws_s3sync /${STORAGE}/${NAME}.tar.gz ${STORAGE}/${NAME}.tar.gz.${CHECKSUM_TYPE}
-  done
+      # Validation type of backup
+      case ${TYPE} in 
+           "default"|"regular_file")
+              regular_file_backup
+           ;;
+           "mysql")
+              sgbd_mysql_backup
+           ;;
+           "postgres")
+              sgbd_postgres_backup
+           ;;
+           *)
+             echo "[ERROR]: TYPE is not definied in JOB => ${NAME}!"
+             exit 0
+           ;;
+      esac
+  done 
 }
+# Input job_def file 
 make_backup ${JOB[*]}
