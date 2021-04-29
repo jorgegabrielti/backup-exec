4f2c062d93047dc594f1a0f57a7153bad11f3214
jorgegabrielti <jorgegabriel.ti@gmail.com>
2021-04-28 23:26:41 -0300

    [update] Improve with idempotency the funcion/backup_agent_apply.sh

--- function/backup_agent_apply.sh
+++ function/backup_agent_apply.sh
@@ -1,7 +1,9 @@
 # Test: [OK]
 backup_agent_apply () {
-    pssh -P --timeout=0 --hosts=${PSSH_HOSTS} \
-    'echo -e "\n### Backup keeper - Cron job\n\
+    # This command is idempotent and validates if the job is already configured in the crontab
+    pssh --timeout=0 --hosts=${PSSH_HOSTS} \
+    'grep -F "@daily root bash /tmp/backup-agent.sh /tmp/*.conf.db > /dev/null 2>&1" /etc/crontab\
+    || echo -e "\n### Backup keeper - Cron job\n\
 @daily root bash /tmp/backup-agent.sh /tmp/*.conf.db > /dev/null 2>&1" \
     >> /etc/crontab'
 }
