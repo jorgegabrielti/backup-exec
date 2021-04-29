61411163df47abdadd8f7209bfd9e51408858078
jorgegabrielti <jorgegabriel.ti@gmail.com>
2021-04-28 22:22:31 -0300

    [update] function/backup_agent_apply.sh

--- function/backup_agent_apply.sh
+++ function/backup_agent_apply.sh
@@ -1,4 +1,7 @@
 # Test: [OK]
 backup_agent_apply () {
-    pssh -P --timeout=0 --hosts=${PSSH_HOSTS}  'bash -x /tmp/backup-agent.sh /tmp/*.conf.db'
+    pssh -P --timeout=0 --hosts=${PSSH_HOSTS} \
+    'echo -e "\n### Backup keeper - Cron job\n\
+@daily root bash /tmp/backup-agent.sh /tmp/*.conf.db > /dev/null 2>&1" \
+    >> /etc/crontab'
 }
