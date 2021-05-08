456c33c33371a96e354b4164e52a0906b2b155f1
jorgegabrielti <jorgegabriel.ti@gmail.com>
2021-05-07 21:30:43 -0300

    [update] backup_job_discovery.sh

--- backup_job_discovery.sh
+++ backup_job_discovery.sh
@@ -2,6 +2,8 @@
 source backup.conf.db
 FILE=$1
 
+Z_HOST=$(cut -d':' -f2- sample.conf.db | tr -s '[:blank:]' '\n' | grep -E 'Z_HOST' | cut -d'=' -f2)
+
 for JSON in $(cat ${FILE} | tr -s '[:blank:]' '\n' | grep -E 'NAME=' | cut -d'=' -f2 | sort); do
     JSON_FORMAT="${JSON_FORMAT},"'{"{#JOB}":"'${JSON}'"}'
 done
