0b25519ecb90f2bd4607d568eb3aa9435a05a105
jorgegabrielti <jorgegabriel.ti@gmail.com>
2021-05-07 22:17:08 -0300

    [fix] backup_job_discovery.sh in json format

--- backup_job_discovery.sh
+++ backup_job_discovery.sh
@@ -2,12 +2,12 @@
 source backup.conf.db
 FILE=$1
 
-Z_HOST=$(cut -d':' -f2- sample.conf.db | tr -s '[:blank:]' '\n' | grep -E 'Z_HOST' | cut -d'=' -f2)
+Z_HOST=$(zabbix_agentd --help | grep default | cut -d':' -f2 | tr -d '"|)|(' | xargs grep -E '^Hostname' | cut -d'=' -f2)
+
 
 for JSON in $(cat ${FILE} | tr -s '[:blank:]' '\n' | grep -E 'NAME=' | cut -d'=' -f2 | sort); do
     JSON_FORMAT="${JSON_FORMAT},"'{"{#JOB}":"'${JSON}'"}'
 done
-echo '{"data":['${JSON_FORMAT#,}' ]}'
 
 # Trigger to discovery jobs
-/usr/bin/zabbix_sender -z ${Z_SERVER} -p ${Z_SERVER_PORT} -s "${Z_HOST}" -k backup.job.discovery -o "'{"data":['${JSON_FORMAT#,}' ]}'"
+/usr/bin/zabbix_sender -z ${Z_SERVER} -p ${Z_SERVER_PORT} -s "${Z_HOST}" -k backup.job.discovery -o '{"data":['${JSON_FORMAT#,}' ]}'
